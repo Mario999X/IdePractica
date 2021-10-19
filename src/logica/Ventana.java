5896adbcbb445518f2cc03a25f57562c44a265a0
MarioGonzalezResa <Mario999X>
2021-10-19 13:19:12 +0200

    Ahora agregada la funcionalidad imprimir

--- src/logica/Ventana.java
+++ src/logica/Ventana.java
@@ -2,22 +2,44 @@ package logica;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana extends JFrame {
 
-    private BarraDeHerramientas menu;
     private AreaTexto areaTexto;
     private Terminal areaTerminal;
     private ExploradorArchivos exploradorArchivos;
     private JPanel panelPrincipal;
 
+    //Menu
+    private JMenuBar menu;
+    private JMenu menuArchivo;
+    private JMenuItem menuArchivoOpcionNuevo;
+    private JMenuItem menuArchivoOpcionAbrir;
+    private JFileChooser menuArchivoOpcionAbrirAccion;
+    private JMenuItem menuArchivoOpcionGuardar;
+    private JMenuItem menuArchivoOpcionGuardarComo;
+    private JMenuItem menuArchivoOpcionImprimir;
+
+    private JMenu menuEdicion;
+    private JMenuItem menuEdicionOpcionDeshacer;
+    private JMenuItem menuEdicionOpcionCopiar;
+    private JMenuItem menuEdicionOpcionCortar;
+    private JMenuItem menuEdicionOpcionPegar;
+    private JMenuItem menuEdicionOpcionEliminar;
+
+    private JMenu menuAyuda;
+    private JMenuItem menuAyudaOpcionAcercaDe;
+    private JMenuItem menuAyudaOpcionVerAyuda;
+    
+
     public Ventana() {
-        menu = new BarraDeHerramientas();
         areaTerminal = new Terminal();
         areaTexto = new AreaTexto();
         exploradorArchivos = new ExploradorArchivos();
+        initBarraDeHerramientas();
         initComponents();
-
     }
 
     public void initComponents() {
@@ -26,7 +48,7 @@ public class Ventana extends JFrame {
         panelPrincipal.setLayout(new BorderLayout());
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(500, 500));
-        setJMenuBar(menu.getMenu());
+        setJMenuBar(menu);
         panelPrincipal.add(areaTexto, BorderLayout.CENTER);
         panelPrincipal.add(areaTerminal, BorderLayout.SOUTH);
         areaTerminal.setPreferredSize(new Dimension(200,200));
@@ -37,6 +59,65 @@ public class Ventana extends JFrame {
         pack();
     }
 
+        private void initBarraDeHerramientas() {
+
+            menu = new JMenuBar();
+
+            //Menu Archivo
+            menuArchivo = new JMenu();
+            menuArchivo.setText("Archivo");
+
+            menuArchivoOpcionNuevo = new JMenuItem("Nuevo");
+            menuArchivoOpcionAbrir = new JMenuItem("Abrir");
+            menuArchivoOpcionGuardar = new JMenuItem("Guardar");
+            menuArchivoOpcionGuardarComo = new JMenuItem("Guardar Como");
+            menuArchivoOpcionImprimir = new JMenuItem("Imprimir");
+
+            menuArchivo.add(menuArchivoOpcionNuevo);
+            menuArchivo.add(menuArchivoOpcionAbrir);
+            menuArchivo.add(menuArchivoOpcionGuardar);
+            menuArchivo.add(menuArchivoOpcionGuardarComo);
+            menuArchivo.add(menuArchivoOpcionImprimir);
+
+            menuArchivoOpcionImprimir.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    areaTexto.print();
+                }
+            });
+
+            //Menu Edicion
+            menuEdicion = new JMenu();
+            menuEdicion.setText("Edición");
+
+            menuEdicionOpcionDeshacer = new JMenuItem("Deshacer");
+            menuEdicionOpcionCopiar = new JMenuItem("Copiar");
+            menuEdicionOpcionCortar = new JMenuItem("Cortar");
+            menuEdicionOpcionPegar = new JMenuItem("Pegar");
+            menuEdicionOpcionEliminar = new JMenuItem("Eliminar");
+
+            menuEdicion.add(menuEdicionOpcionDeshacer);
+            menuEdicion.add(menuEdicionOpcionCopiar);
+            menuEdicion.add(menuEdicionOpcionCortar);
+            menuEdicion.add(menuEdicionOpcionPegar);
+            menuEdicion.add(menuEdicionOpcionEliminar);
+
+            // Menu Ayuda
+            menuAyuda = new JMenu();
+            menuAyuda.setText("Ayuda");
+
+            menuAyudaOpcionAcercaDe = new JMenuItem("Acerca de");
+            menuAyudaOpcionVerAyuda = new JMenuItem("Ver Ayuda");
+
+            menuAyuda.add(menuAyudaOpcionAcercaDe);
+            menuAyuda.add(menuAyudaOpcionVerAyuda);
+
+            //Agrego menu
+            menu.add(menuArchivo);
+            menu.add(menuEdicion);
+            menu.add(menuAyuda);
+
+    }
 }
 
 
